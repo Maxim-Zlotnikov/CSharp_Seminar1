da07184aeb45bdacb22728629d2c1e0556ccd715
Maxim Zlotnikov <mzlotnikov@mail.ru>
2022-08-13 16:04:52 +0300

    Example 00

---
+++ Example00/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число
+// и выдает его квдрат (число, умноженное на само себя).
+
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Квадрат числа {number} равен - {number * number}");
+System.Console.WriteLine($"Квадрат числа {number} равен - {Math.Pow(number, 2)}");
