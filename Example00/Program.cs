2d8139c54a674a915241a7a802522ddfce48a24c
Maxim Zlotnikov <mzlotnikov@mail.ru>
2022-08-13 20:42:57 +0300

    End Seminar 1

--- Example00/Program.cs
+++ Example00/Program.cs
@@ -2,7 +2,7 @@
 // и выдает его квдрат (число, умноженное на само себя).
 
 System.Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()!);
 
 System.Console.WriteLine($"Квадрат числа {number} равен - {number * number}");
 System.Console.WriteLine($"Квадрат числа {number} равен - {Math.Pow(number, 2)}");
